dfa4388e5a7e9058ebc0f22936115c42f52cc274
CepotOi <rizkihutama69@gmail.com>
2022-01-28 18:44:40 +0700

    async await fetch data from rest api

--- index.js
+++ index.js
@@ -1,53 +1,90 @@
-function showCountries() {
-  const countriesEndPoint = 'https://restcountries.com/v3.1/all';
+const countriesEndPoint = 'https://restcountries.com/v3.1/alls';
 
-  /**
-   ** Ajax Request   
-   */
-  // const xhr = new XMLHttpRequest();
+/**
+ ** AJAX and Fetch API
+*/
+// function showCountries() {
 
-  // xhr.open('GET', countriesEndPoint, true);
+//   /**
+//    ** Ajax Request
+//    */
+//   // const xhr = new XMLHttpRequest();
 
-  // xhr.onload = () => {
-  //   if (xhr.status === 200) {
-  //     const countries = JSON.parse(xhr.response);
-  //     const countriesCard = document.querySelector('.row-countries');
+//   // xhr.open('GET', countriesEndPoint, true);
 
-  //     countries.forEach(country => {
-  //       const col = document.createElement('div');
-  //       col.classList.add('col-md-3');
-  //       const card = document.createElement('div');
-  //       card.classList.add('card', 'mt-3');
-  //       card.innerHTML = `
-  //         <div class="card-image">
-  //           <img src="${country.flags.png}" class="countries-img">
-  //         </div>
-  //         <div class="card-content">
-  //           <h3>${country.name.common}</h3>
-  //           <p>${country.name.official}</p>
-  //           <p>${country.capital}</p>
-  //         </div>
-  //       `;
-  //       col.appendChild(card);
-  //       countriesCard.appendChild(col);
-  //     });
-  //   }
-  // };
+//   // xhr.onload = () => {
+//   //   if (xhr.status === 200) {
+//   //     const countries = JSON.parse(xhr.response);
+//   //     const countriesCard = document.querySelector('.row-countries');
 
-  // xhr.send();
+//   //     countries.forEach(country => {
+//   //       const col = document.createElement('div');
+//   //       col.classList.add('col-md-3');
+//   //       const card = document.createElement('div');
+//   //       card.classList.add('card', 'mt-3');
+//   //       card.innerHTML = `
+//   //         <div class="card-image">
+//   //           <img src="${country.flags.png}" class="countries-img">
+//   //         </div>
+//   //         <div class="card-content">
+//   //           <h3>${country.name.common}</h3>
+//   //           <p>${country.name.official}</p>
+//   //           <p>${country.capital}</p>
+//   //         </div>
+//   //       `;
+//   //       col.appendChild(card);
+//   //       countriesCard.appendChild(col);
+//   //     });
+//   //   }
+//   // };
 
-  /**
-   * Fetch (Promise)
-  */
-  fetch(countriesEndPoint)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('Something went wrong on api server!');
-      }
-    })
-    .then(countries => {
+//   // xhr.send();
+
+//   /**
+//    * Fetch (Promise)
+//   */
+//   // fetch(countriesEndPoint)
+//   //   .then(response => {
+//   //     if (response.status === 200) {
+//   //       return response.json();
+//   //     } else {
+//   //       throw new Error('Something went wrong on api server!');
+//   //     }
+//   //   })
+//   //   .then(countries => {
+//   //     const countriesCard = document.querySelector('.row-countries');
+
+//   //     countries.forEach(country => {
+//   //       const col = document.createElement('div');
+//   //       col.classList.add('col-md-3');
+//   //       const card = document.createElement('div');
+//   //       card.classList.add('card', 'mt-3');
+//   //       card.innerHTML = `
+//   //           <div class="card-image">
+//   //             <img src="${country.flags.png}" class="countries-img">
+//   //           </div>
+//   //           <div class="card-content">
+//   //             <h3>${country.name.common}</h3>
+//   //             <p>${country.name.official}</p>
+//   //             <p>${country.capital}</p>
+//   //           </div>
+//   //         `;
+//   //       col.appendChild(card);
+//   //       countriesCard.appendChild(col);
+//   //     });
+//   //   })
+//   //   .catch(error => console.error(error));
+// }
+
+/**
+ ** Async Await
+ * 1.53s
+ */
+async function showCountries() {
+  try {
+    const response = await fetch(countriesEndPoint);
+    if (response.status === 200) {
+      const countries = await response.json();
       const countriesCard = document.querySelector('.row-countries');
 
       countries.forEach(country => {
@@ -56,18 +93,21 @@ function showCountries() {
         const card = document.createElement('div');
         card.classList.add('card', 'mt-3');
         card.innerHTML = `
-            <div class="card-image">
-              <img src="${country.flags.png}" class="countries-img">
-            </div>
-            <div class="card-content">
-              <h3>${country.name.common}</h3>
-              <p>${country.name.official}</p>
-              <p>${country.capital}</p>
-            </div>
-          `;
+        <div class="card-image">
+          <img src="${country.flags.png}" class="countries-img">
+        </div>
+        <div class="card-content">
+          <h3>${country.name.common}</h3>
+          <p>${country.capital}</p>
+        </div>
+      `;
         col.appendChild(card);
         countriesCard.appendChild(col);
       });
-    })
-    .catch(error => console.error(error));
+    } else {
+      throw new Error('Something went wrong on api server!');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
