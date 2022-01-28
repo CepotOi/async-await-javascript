42f07efd8c7828229ea48ea375c6841652f0a3e6
CepotOi <rizkihutama69@gmail.com>
2022-01-28 18:10:44 +0700

    fetch (promise)

--- index.js
+++ index.js
@@ -4,13 +4,50 @@ function showCountries() {
   /**
    ** Ajax Request   
    */
-  const xhr = new XMLHttpRequest();
+  // const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', countriesEndPoint, true);
+  // xhr.open('GET', countriesEndPoint, true);
 
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      const countries = JSON.parse(xhr.response);
+  // xhr.onload = () => {
+  //   if (xhr.status === 200) {
+  //     const countries = JSON.parse(xhr.response);
+  //     const countriesCard = document.querySelector('.row-countries');
+
+  //     countries.forEach(country => {
+  //       const col = document.createElement('div');
+  //       col.classList.add('col-md-3');
+  //       const card = document.createElement('div');
+  //       card.classList.add('card', 'mt-3');
+  //       card.innerHTML = `
+  //         <div class="card-image">
+  //           <img src="${country.flags.png}" class="countries-img">
+  //         </div>
+  //         <div class="card-content">
+  //           <h3>${country.name.common}</h3>
+  //           <p>${country.name.official}</p>
+  //           <p>${country.capital}</p>
+  //         </div>
+  //       `;
+  //       col.appendChild(card);
+  //       countriesCard.appendChild(col);
+  //     });
+  //   }
+  // };
+
+  // xhr.send();
+
+  /**
+   * Fetch (Promise)
+  */
+  fetch(countriesEndPoint)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong on api server!');
+      }
+    })
+    .then(countries => {
       const countriesCard = document.querySelector('.row-countries');
 
       countries.forEach(country => {
@@ -19,20 +56,18 @@ function showCountries() {
         const card = document.createElement('div');
         card.classList.add('card', 'mt-3');
         card.innerHTML = `
-          <div class="card-image">
-            <img src="${country.flags.png}" class="countries-img">
-          </div>
-          <div class="card-content">
-            <h3>${country.name.common}</h3>
-            <p>${country.name.official}</p>
-            <p>${country.capital}</p>
-          </div>
-        `;
+            <div class="card-image">
+              <img src="${country.flags.png}" class="countries-img">
+            </div>
+            <div class="card-content">
+              <h3>${country.name.common}</h3>
+              <p>${country.name.official}</p>
+              <p>${country.capital}</p>
+            </div>
+          `;
         col.appendChild(card);
         countriesCard.appendChild(col);
       });
-    }
-  };
-
-  xhr.send();
+    })
+    .catch(error => console.error(error));
 }
